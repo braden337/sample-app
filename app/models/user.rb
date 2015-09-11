e90a79d3bd26d4a3c165273830aff0ba1af10a5a
Braden Jonsson <braden337@gmail.com>
2015-09-11 21:50:24 +0000

    Work on chapter 6 lol

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
-
+  validates :email, format: { with: // }
 end
