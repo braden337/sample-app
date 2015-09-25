81fcb21cf8b182e7ed8dff01ed4080240510ce10
Braden Jonsson <braden337@gmail.com>
2015-09-25 17:07:03 +0000

    Add test for error message html elements

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div.alert-danger', text: 'The form contains 4 errors.'
+    assert_select 'div#error_explanation ul li', count: 4
+    assert_select 'li', text: 'Name can\'t be blank', count: 1
+    assert_select 'li', text: 'Email is invalid', count: 1
+    assert_select 'li', text: 'Password confirmation doesn\'t match Password', count: 1
+    assert_select 'li', text: 'Password is too short (minimum is 6 characters)', count: 1
   end
 
   test "valid signup information" do
@@ -22,6 +28,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_select 'div.alert-success', text: 'Welcome to the Sample App!'
   end
 
 end
